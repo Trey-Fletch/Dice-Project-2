2595bae71031ab3fd70607d65fab774abee3fdae
Trey-Fletch <72419334+Trey-Fletch@users.noreply.github.com>
2020-11-22 16:42:00 -0600

    Initial Commit

---
+++ Dice Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice Project
+//
+//  Created by Student on 11/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

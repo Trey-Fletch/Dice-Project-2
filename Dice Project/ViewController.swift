b58d87d161a37a206ffe50f899cc8c3759d7b331
Trey-Fletch <72419334+Trey-Fletch@users.noreply.github.com>
2020-12-09 18:43:05 -0600

    DICE APP

--- Dice Project/ViewController.swift
+++ Dice Project/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let diceArray = ["dice0", "dice1", "dice2", "dice3", "dice4", "dice5"]
+    
+    var randomDiceIndex : Int = 0
+    
+    
+    @IBOutlet weak var diceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func askButtonPressed(_ sender: Any) {
+        newDiceImage()
+    }
+    
+    
+    func newDiceImage() {
+        randomDiceIndex = Int.random(in: 0...5)
+        diceImageView.image = UIImage(named:
+        diceArray[randomDiceIndex])
+       
+        
+      
+            }
+    
 }
-
